f2efe57dcd2803580ca9d4daafa54340f13d3b90
theplayer06 <manekiswap@gmail.com>
2021-06-24 21:31:22 +0700

    Update colors and button styles

--- src/pages/sample/index.tsx
+++ src/pages/sample/index.tsx
@@ -10,18 +10,41 @@ export default function SamplePage() {
         alignItems: 'center',
         justifyContent: 'center',
         flexDirection: 'column',
-        backgroundColor: 'white',
+        backgroundColor: 'dark.500',
       }}
     >
-      <Flex sx={{ alignItems: 'center' }}>
+      <Flex sx={{ alignItems: 'center', justifyContent: 'space-between' }}>
         <Heading as="h1" variant="styles.h1" sx={{ color: 'white' }} />
         <Button variant="buttons.primary">Large button</Button>
+        <Button variant="buttons.primary">
+          <ArrowUp />
+          Large button
+        </Button>
+        <Button variant="buttons.secondary">Large button</Button>
         <Button variant="buttons.ghost">Large button</Button>
         <Button variant="buttons.link">Large button</Button>
-        <Button variant="buttons.small-primary">Small button</Button>
-        <Button variant="buttons.small-ghost">Small button</Button>
-        <Button variant="buttons.small-link">Small button</Button>
         <Button variant="buttons.icon">
+          <ArrowUp />
+        </Button>
+      </Flex>
+      <Flex sx={{ alignItems: 'center', justifyContent: 'space-between' }}>
+        <Button variant="buttons.small-primary" disabled>
+          Small button
+        </Button>
+        <Button variant="buttons.small-primary" disabled>
+          Small button
+          <ArrowUp />
+        </Button>
+        <Button variant="buttons.small-secondary" disabled>
+          Small button
+        </Button>
+        <Button variant="buttons.small-ghost" disabled>
+          Small button
+        </Button>
+        <Button variant="buttons.small-link" disabled>
+          Small button
+        </Button>
+        <Button variant="buttons.icon" disabled>
           <ArrowUp />
         </Button>
       </Flex>
