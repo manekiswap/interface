d396591afc9158ee9d8eae181a40dbd114becc0a
theplayer06 <manekiswap@gmail.com>
2021-08-02 16:48:06 +0700

    Memo pair selector

--- src/hooks/usePoolPair.ts
+++ src/hooks/usePoolPair.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
 import { ETH, Token } from '../constants/token';
 
@@ -12,14 +12,16 @@ export default function usePoolPair(): {
   const [token0, setToken0] = useState(ETH);
   const [token1, setToken1] = useState<Token | undefined>(undefined);
 
-  return {
-    token0,
-    token1,
-    updateToken0: setToken0,
-    updateToken1: setToken1,
-    reset: () => {
-      setToken0(ETH);
-      setToken1(undefined);
-    },
-  };
+  return useMemo(() => {
+    return {
+      token0,
+      token1,
+      updateToken0: setToken0,
+      updateToken1: setToken1,
+      reset: () => {
+        setToken0(ETH);
+        setToken1(undefined);
+      },
+    };
+  }, [token0, token1]);
 }
--- src/hooks/useSwapPair.ts
+++ src/hooks/useSwapPair.ts
@@ -1,33 +1,16 @@
+import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 
-import { SupportedChainId } from '../constants/chains';
 import { Token } from '../constants/token';
 import { selectors } from '../reducers';
 
 export default function useSwapPair(): { token0?: Token; token1?: Token } {
   const { token0, token1 } = useSelector(selectors.swap.selectSwapPair);
-  return {
-    token0:
-      token0 &&
-      new Token(
-        token0.chainId,
-        token0.address,
-        token0.decimals,
-        token0.symbol,
-        undefined,
-        undefined,
-        token0.chainId === SupportedChainId.MAINNET && token0.address === '',
-      ),
-    token1:
-      token1 &&
-      new Token(
-        token1.chainId,
-        token1.address,
-        token1.decimals,
-        token1.symbol,
-        undefined,
-        undefined,
-        token1.chainId === SupportedChainId.MAINNET && token1.address === '',
-      ),
-  };
+
+  return useMemo(() => {
+    return {
+      token0: token0 && Token.fromShortToken(token0),
+      token1: token1 && Token.fromShortToken(token1),
+    };
+  }, [token0, token1]);
 }
