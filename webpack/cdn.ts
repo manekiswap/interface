dcc11222aee1f662e74c914ee819cd5f366d7ed6
theplayer06 <manekiswap@gmail.com>
2021-08-15 22:32:10 +0700

    Fix cdn path

--- webpack/cdn.ts
+++ webpack/cdn.ts
@@ -15,14 +15,12 @@ const { cdnPaths, externals } = (function () {
       `https://unpkg.com/react-dom@${dependencies['react-dom']}/umd/react-dom.production.min.js`,
       `https://unpkg.com/i18next@${dependencies['i18next']}/dist/umd/i18next.min.js`,
       `https://unpkg.com/react-i18next@${dependencies['react-i18next']}/react-i18next.min.js`,
-      `https://unpkg.com/bignumber.js@${dependencies['bignumber.js']}/bignumber.min.js`,
     ],
     externals: {
       i18next: 'i18next',
       react: 'React',
       'react-dom': 'ReactDOM',
       'react-i18next': 'ReactI18next',
-      'bignumber.js': 'BigNumber',
     },
   };
 })();
